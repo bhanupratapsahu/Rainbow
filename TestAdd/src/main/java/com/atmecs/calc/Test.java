99a8a120693fb9dee7b6c36f1672a85a30f68a25
Sachin <sachinraj245@gmail.com>
2019-06-19 16:43:34 +0530

    maven project pushed to nexus repo

---
+++ TestAdd/src/main/java/com/atmecs/calc/Test.java
@@ -0,0 +1,12 @@
+package com.atmecs.calc;
+
+import com.atmecs.calculator.Addition;
+
+public class Test {
+	
+public static void main(String[] args) {
+	Addition add = new Addition();
+	System.out.println(add.additionOfNumbers(10,25,4,15));
+}
+
+}
